0c1f86c3f6e4476765382359173f3ac9dd5509d4
calebtc <calebthecurious@gmail.com>
2023-09-24 13:39:33 +0800

    3rd commit - authentication

---
+++ middleware.ts
@@ -0,0 +1,9 @@
+import { authMiddleware } from "@clerk/nextjs";
+
+export default authMiddleware({
+    publicRoutes: ['/test']
+});
+
+export const config = {
+    matcher: ["/((?!.*\\..*|_next).*)","/","/(api|trpc)(.*)"],
+}
