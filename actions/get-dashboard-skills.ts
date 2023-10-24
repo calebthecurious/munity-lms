fd27a503874b1f57d7224ca6c455e1c2e9ad75b9
calebtc <calebthecurious@gmail.com>
2023-10-24 21:22:49 +1100

    29th commit - dashboard status added

---
+++ actions/get-dashboard-skills.ts
@@ -0,0 +1,58 @@
+import { Category, Chapter, Skill } from "@prisma/client";
+
+import { db } from "@/lib/db";
+import { getProgress } from "@/actions/get-progress";
+
+type SkillWithProgressWithCategory = Skill & {
+  category: Category;
+  chapters: Chapter[];
+  progress: number | null;
+};
+
+type DashboardSkills = {
+  completedSkills: SkillWithProgressWithCategory[];
+  skillsInProgress: SkillWithProgressWithCategory[];
+}
+
+export const getDashboardSkills = async (userId: string): Promise<DashboardSkills> => {
+  try {
+    const purchasedSkills = await db.purchase.findMany({
+      where: {
+        userId: userId,
+        },
+        select: {
+            skill: {
+            include: {
+                category: true,
+                chapters: {
+                where: {
+                    isPublished: true,
+                }
+                }
+            }
+            }
+        }
+    });
+
+    const skills = purchasedSkills.map((purchase) => purchase.skill) as SkillWithProgressWithCategory[];
+
+    for (let skill of skills) {
+      const progress = await getProgress(userId, skill.id);
+      skill["progress"] = progress;
+    }
+
+    const completedSkills = skills.filter((skill) => skill.progress === 100);
+    const skillsInProgress = skills.filter((skill) => (skill.progress ?? 0) < 100);
+
+    return {
+      completedSkills,
+      skillsInProgress,
+    }
+  } catch (error) {
+    console.log("[GET_DASHBOARD_COURSES]", error);
+    return {
+      completedSkills: [],
+      skillsInProgress: [],
+    }
+  }
+}
---
+++ app/(dashboard)/(routes)/(root)/page.tsx
@@ -0,0 +1,44 @@
+import { getDashboardSkills } from "@/actions/get-dashboard-skills";
+import { SkillsList } from "@/components/skill-list";
+import { auth } from "@clerk/nextjs"
+import { CheckCircle, Clock } from "lucide-react";
+import { redirect } from "next/navigation";
+import { InfoCard } from "./_components/info-card";
+
+export default async function Dashboard() {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const {
+    completedSkills,
+    skillsInProgress
+  } = await getDashboardSkills(userId);
+  
+  
+  return (
+      <div className="p-6 space-y-4">
+        <div className="grid  grid-cols-1 sm:grid-cols-2 gap-4">
+          <InfoCard 
+            icon={Clock}
+            label="In Progress"
+            numberOfItems={skillsInProgress.length}
+          />
+          <InfoCard 
+            icon={CheckCircle}
+            label="In Completed"
+            numberOfItems={skillsInProgress.length}
+            variant="success"
+          />
+        </div>
+        
+        <SkillsList
+            items={[...skillsInProgress, ...completedSkills]}
+          />
+
+        
+      </div>
+    )
+}
