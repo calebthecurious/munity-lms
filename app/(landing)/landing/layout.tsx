28545531dfc0085a388b0fa58fb60575929106bb
calebtc <calebthecurious@gmail.com>
2023-11-06 21:26:35 +1100

    30th commit - about page added

--- app/(landing)/landing/layout.tsx
+++ app/(landing)/landing/layout.tsx
@@ -4,16 +4,16 @@ import { Navbar } from "@/app/(dashboard)/_components/Navbar";
 const MarketingLayout = ({
     children
 }: {
-  children: React.ReactNode;
-}) => {
-  return ( 
-    <div className="dark:bg-[#1F1F1F]">
-      <Navbar />
-      <main className="h-full pt-40">
-        {children}
-      </main>
-    </div>
-   );
-}
- 
+    children: React.ReactNode;
+    }) => {
+    return ( 
+        <div className="dark:bg-[#1F1F1F]">
+        <Navbar />
+        <main className="h-full pt-40">
+            {children}
+        </main>
+        </div>
+    );
+    }
+    
 export default MarketingLayout;
